45737c4af3a55c56126dd493297201f182c7904a
Darsi Sriguru Aakash <66508746+Aakashdarsi@users.noreply.github.com>
2023-08-29 15:15:49 +0530

    For Loop added

---
+++ src/forLoop.java
@@ -0,0 +1,12 @@
+// This program is used for basic for loop
+public class forLoop {
+    public static void main(String[] args) {
+        int x = 0;
+        for(int counter = 0;counter<100;counter++)
+        {
+            x+=1;
+        }
+        System.out.println(x);
+
+    }
+}
